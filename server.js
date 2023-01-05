fa4a802b0aa25a69b86f56f22c5917a414f5bf46
Ravi Nagi <rasvindra@hotmail.com>
2023-01-05 09:40:13 -0800

    fixed spelling errors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 
-const sequelize = require("./config/connection.js");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
